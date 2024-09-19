9273238d8411233fe882fc25eec02e0dab75cba9
elankathir <esekar@diligentsquad.com>
2024-09-19 16:18:31 +0530

    diffrent ways of setting headers to request

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -28,7 +28,21 @@ export class TaskService {
   }
 
   getAllTasks() {
-    return this.http.get<{[key : string] : Task }>('https://angularhttpclient-6d23d-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json')
+    //setting headers to get request
+    let headers = new HttpHeaders();
+    headers = headers.set('content-type', 'application/json')
+    headers = headers.set('content-type', 'text/html')
+    headers = headers.set('allow-edit','*')
+    /*
+    headers = headers.append('content-type', 'application/json')
+    headers = headers.append('content-type', 'text/html')
+    headers = headers.append('allow-edit','*')
+    */
+   /*
+   Note : when we use set method if key value is same it will replace existing value to new one
+   whereas in append method it will append the value with existing one
+   */
+    return this.http.get<{[key : string] : Task }>('https://angularhttpclient-6d23d-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json',{headers : headers})
     .pipe(map((res) => {
       const tasks : Task[] = [];
 
