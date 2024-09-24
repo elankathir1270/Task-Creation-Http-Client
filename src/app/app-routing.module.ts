1fb893c8d260d727280622f160bd95b03a837929
elankathir <esekar@diligentsquad.com>
2024-09-24 14:58:38 +0530

    Implementation of route guard for auth functions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { canActivate } from './route-guards/authGuard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path : 'login', component : LoginComponent},
-  {path: 'dashboard', component : DashboardComponent}
+  {path: 'dashboard', component : DashboardComponent, canActivate : [canActivate]}
 ];
 
 @NgModule({
